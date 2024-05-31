979bb8f2f64e18501e0ff969c08a4bfda156f211
BenChen <tttw216@gmail.com>
2024-05-31 09:58:49 +0800

    [Feat] dateTime test

--- common/src/main/java/com/hahow/common/extension/DateTimeExt.kt
+++ common/src/main/java/com/hahow/common/extension/DateTimeExt.kt
@@ -1,5 +1,6 @@
 package com.hahow.common.extension
 
+import android.icu.util.LocaleData
 import com.hahow.common.DateTimeFormatPattern.DATE_FORMAT
 import com.hahow.common.DateTimeFormatPattern.DATE_FORMAT_EN
 import com.hahow.common.DateTimeFormatPattern.DATE_TIME_FORMAT
@@ -12,6 +13,7 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import java.util.Locale
 
 
@@ -45,35 +47,8 @@ val String.localDateTimeFromISO: LocalDateTime
 val String.localDateFromISO: LocalDate
     get() = LocalDate.parse(this, DateTimeFormatter.ISO_DATE)
 
-
-fun LocalDateTime.isBetween(
-    startDateTime: LocalDateTime,
-    endDateTime: LocalDateTime
-): Boolean =
-    this.isAfter(startDateTime) && this.isBefore(endDateTime)
-
-
-fun LocalTime.textWithoutSec(): String = format(
-    DateTimeFormatter.ofPattern(TIME_FORMAT_H_M)
-)
-
-
-fun LocalTime.formatToFileName(): String = format(
-    DateTimeFormatter.ofPattern(TIME_FORMAT_H_M_FILE_NAME)
-)
-
-/**
- * @return
- * 英文: Dec/13/2022 08:08:08
- * 其他: 2022-12-13 08:08:08
- */
-
-fun LocalDateTime.text(locale: Locale = Locale.CHINESE): String {
-    val pattern = when (locale.language) {
-        "en" -> DATE_TIME_FORMAT_EN
-        else -> DATE_TIME_FORMAT
-    }
-    return format(DateTimeFormatter.ofPattern(pattern, locale))
+fun LocalDateTime.betweenDay(dueDateTime: LocalDateTime): Long {
+    return this.until(dueDateTime, ChronoUnit.DAYS)
 }
 
 /**
--- common/src/test/java/com/hahow/common/extension/DateTimeExtTest.kt
+++ common/src/test/java/com/hahow/common/extension/DateTimeExtTest.kt
@@ -2,6 +2,7 @@ package com.hahow.common.extension
 
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import java.time.temporal.ChronoUnit
 
 class DateTimeExtTest {
     private val jsonDateTime = "2023-07-14T15:59:02Z"
@@ -9,10 +10,50 @@ class DateTimeExtTest {
     @Test
     @DisplayName("測試 String to LocalDate")
     fun stringToLocalDateTest() {
-        val dateTime = jsonDateTime.localDateTimeFromISO.toLocalDate()
+        val localDate = jsonDateTime.localDateTimeFromISO.toLocalDate()
 
-        dateTime.year shouldBe 2023
-        dateTime.month.value shouldBe 7
-        dateTime.dayOfMonth shouldBe 14
+        localDate.year shouldBe 2023
+        localDate.month.value shouldBe 7
+        localDate.dayOfMonth shouldBe 14
+    }
+
+    @Test
+    @DisplayName("測試 String to LocalTime")
+    fun stringToLocalTimeTest() {
+        val localTime = jsonDateTime.localDateTimeFromISO.toLocalTime()
+
+        localTime.hour shouldBe 15
+        localTime.minute shouldBe 59
+        localTime.second shouldBe 2
+    }
+
+    @Test
+    @DisplayName("測試 String to LocalDateTime")
+    fun stringToLocalDateTimeTest() {
+        val localDateTime = jsonDateTime.localDateTimeFromISO
+
+        localDateTime.year shouldBe 2023
+        localDateTime.month.value shouldBe 7
+        localDateTime.dayOfMonth shouldBe 14
+        localDateTime.hour shouldBe 15
+        localDateTime.minute shouldBe 59
+        localDateTime.second shouldBe 2
+    }
+
+    @Test
+    @DisplayName("測試 LocalDateTime Format")
+    fun dateFormatTest() {
+        val localDate = jsonDateTime.localDateTimeFromISO.toTimestamp().toLocalDate().text()
+
+        localDate shouldBe "2023-07-14"
+    }
+
+    @Test
+    @DisplayName("測試相差天數")
+    fun betweenDayTest() {
+        val fakeTodayDateTime = "2024-05-31T15:59:02Z".localDateTimeFromISO
+        val dueDateTime = "2024-06-01T15:59:02Z".localDateTimeFromISO
+
+        fakeTodayDateTime.betweenDay(dueDateTime) shouldBe 1
     }
 }
