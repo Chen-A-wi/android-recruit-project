4f09df22feb199a4c42d81e6338a7d6e60417f27
Ben <tttw216@gmail.com>
2024-06-04 15:23:56 +0800

    [Fix] layout

--- app/src/main/java/in/hahow/android_recruit_project/ui/widgets/ItemView.kt
+++ app/src/main/java/in/hahow/android_recruit_project/ui/widgets/ItemView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
@@ -46,7 +47,7 @@ fun ItemView(
             .height(120.dp),
     ) {
         Column(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.width(140.dp),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             ImageCover(
@@ -58,7 +59,7 @@ fun ItemView(
             ProgressBarLayout(modifier = Modifier.weight(1f), course = course)
         }
         Column(
-            Modifier.weight(2f)
+            Modifier.fillMaxWidth()
         ) {
             ItemContent(course)
         }
